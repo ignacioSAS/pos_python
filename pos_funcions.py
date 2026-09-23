c2f04c5fa6abd17753a0122d1388bef5494aaee9
ignacioSAS <ignacioguerrero.sas@gmail.com>
2026-09-23 01:20:04 -0600

    first commit pos_funcions

---
+++ pos_funcions.py
@@ -0,0 +1,19 @@
+#pos_fincions
+
+def agregar_producto():
+    print('Agregar producto nuevo')
+
+def buscar_producto():
+    print('Buscando producto')
+
+def inventario_productos():
+    print('inventario de productos')
+
+def ver_productos():
+    print('Mostrando todos los productos')
+
+def venta_producto():
+    print('Vendiendo producto')
+
+def compra_producto():
+    print('Comprando producto')
